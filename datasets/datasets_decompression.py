997b735c00bd3fe134da290aeee44971ab5932b2
Sid Bhatia <sbhatia2@stevens.edu>
2023-05-31 16:02:52 -0400

    Edit author docstring.

--- datasets/datasets_decompression.py
+++ datasets/datasets_decompression.py
@@ -3,7 +3,7 @@ The following Python script decompresses the `.gz` files for the LOB and TAQ dat
 
 Created by Siddharth Bhatia ('24 BSQF, '25 MSML, '25 MFE) on May 31st, 2023.
 
-@author: Sid
+@author: Sid Bhatia
 """
 
 # Import `gzip` to open and write `.gz` files.
