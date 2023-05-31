0eaf860bc0946aa23332b3155a740168cc2ad7f0
Sid Bhatia <sbhatia2@stevens.edu>
2023-05-31 10:11:42 -0400

    Complete datasets_decompression.py.

--- datasets/datasets_decompression.py
+++ datasets/datasets_decompression.py
@@ -24,10 +24,26 @@ def decompress():
             ask price, ask size) for each respective ticker.
     """
 
+    # Initialize lists for each ticker and type for these CSVs.
     ticker_list = ['CVX', 'GME', 'JPM', 'XOM']
     type_list = ['LOB', 'TAQ']
+
+    # Initialize the date for March 14th, 2022
+    date = '20220314'
 
     # Iterate for each ticker and type of data set to result in 8 CSV files.
     for ticker in ticker_list:
         for type in type_list:
-            pass
+            # Use f-string to easily format the file name.
+            file_name = f'{ticker}_{type}_{date}.csv.gz'
+            # Use `with...as` to simplify filestream and open `.gz` files.
+            with gzip.open(filename = file_name, mode = 'rt') as gz_file:
+                # Read compressed data.
+                data = gz_file.read()
+                # Write decompressed data as CSV and index to retrieve precise file name.
+                with open(file = file_name[:-3], mode = 'wt') as out_file:
+                    out_file.write(data)
+
+if __name__ == '__main__':
+    decompress()
+
